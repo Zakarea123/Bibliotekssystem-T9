b9c8c0888d0fbf32465699f36c55667961a1f968
Zakarea123 <zakreaalamor12@gmail.com>
2026-03-18 23:36:24 +0100

    Wire up Catalogservice with loans pages

--- Bibliotekssystem-T9-App/Controllers/LoansController.cs
+++ Bibliotekssystem-T9-App/Controllers/LoansController.cs
@@ -8,10 +8,12 @@ namespace Bibliotekssystem_T9_App.Controllers;
 public class LoansController : Controller
 {
     private readonly LoanApiService  _loanApiService;
+    private readonly CatalogApiService _catalogApiService;
 
-    public LoansController(LoanApiService loanApiService)
+    public LoansController(LoanApiService loanApiService , CatalogApiService catalogApiService)
     {
         _loanApiService = loanApiService;
+        _catalogApiService = catalogApiService;
     }
     
     // GET: Fetches active loans for the current user from LoanService
@@ -19,7 +21,17 @@ public class LoansController : Controller
     public async Task<IActionResult> Index()
     {
         var borrowerId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!); 
-        var loans = await _loanApiService.GetBorrowerLoansAsync(borrowerId);
+        var loans = await _loanApiService.GetBorrowerLoansAsync(borrowerId); 
+        
+        // Fetch item titles for each loan and store in a dictionary
+        var itemTitles = new Dictionary<int, string>();
+        foreach (var loan in loans)
+        { 
+            var item = await _catalogApiService.GetItemAsync(loan.ItemId); 
+            itemTitles[loan.ItemId] = item?.Title ?? $"Objekt {loan.ItemId}";
+        }
+
+        ViewBag.ItemTitles = itemTitles;
         return View(loans);
     }
     
@@ -29,6 +41,15 @@ public class LoansController : Controller
     {
         var borrowerId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!); 
         var history = await _loanApiService.GetBorrowerHistoryAsync(borrowerId);
+        var itemTitles = new Dictionary<int, string>();
+        
+        foreach (var loan in history)
+        { 
+            var item = await _catalogApiService.GetItemAsync(loan.ItemId); 
+            itemTitles[loan.ItemId] = item?.Title ?? $"Objekt {loan.ItemId}";
+        }
+
+        ViewBag.ItemTitles = itemTitles;
         return View(history);
     }
     
@@ -38,6 +59,15 @@ public class LoansController : Controller
     {
         var borrowerId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
         var loans = await _loanApiService.GetBorrowerLoansAsync(borrowerId);
+        
+        var itemTitles = new Dictionary<int, string>();
+        foreach (var loan in loans)
+        { 
+            var item = await _catalogApiService.GetItemAsync(loan.ItemId); 
+            itemTitles[loan.ItemId] = item?.Title ?? $"Objekt {loan.ItemId}";
+        }
+
+        ViewBag.ItemTitles = itemTitles;
         return View(loans);
     }
     
